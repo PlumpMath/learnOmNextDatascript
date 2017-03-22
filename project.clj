621b75f2134f13316801ab8987c2922e9fdd6615
Thinh P. Tran <trphthinh@gmail.com>
2017-03-22 20:53:29 +0700

    Fist commit

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject om-datascript "0.1.0-SNAPSHOT"
+  :description "My first Om program!"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.229"]
+                 [org.omcljs/om "1.0.0-alpha30"]
+                 [datascript "0.13.1"]
+                 [figwheel-sidecar "0.5.0-6" :scope "test"]])
+
+
+
+
